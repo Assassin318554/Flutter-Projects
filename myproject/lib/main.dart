048f9f90671919d22a84b42c6bd6a08e0e4c45c4
Assassin318554 <nahinsikdar123@gmail.com>
2024-05-21 10:30:43 +0600

    a

--- myproject/lib/main.dart
+++ myproject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myproject/HomePage.dart';
+import 'package:flutter/widgets.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,40 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('My first App'),
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.mail),
+          label: const Text('Mail me'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.cyanAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigoAccent,
+        child: const Text('Click'),
+      ),
     );
   }
 }
