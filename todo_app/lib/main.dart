d7890b0247b154683014f3a37bccd0bb7c267ba2
Assassin318554 <nahinsikdar123@gmail.com>
2024-06-28 13:27:33 +0600

    a

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/pages/home/Loading.dart';
 import 'package:todo_app/pages/home/home_page.dart';
-// import 'package:todo_app/pages/home/home_page2.dart';
+import 'package:todo_app/models/task.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox<Task>('tasksBox');
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'To Do App',
+      title: 'Start To Do',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.limeAccent,
--- todo_app/lib/models/task.dart
+++ todo_app/lib/models/task.dart
@@ -1,33 +1,45 @@
-class Task {
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
 
+  @HiveField(2)
   final String? description;
-  bool delete;
+
+  @HiveField(3)
+  bool remove;
+
+  @HiveField(4)
   bool status;
 
+  @HiveField(5)
   final DateTime createdAt;
+
+  @HiveField(6)
   DateTime updatedAt;
 
   Task({
     required this.id,
     required this.title,
     this.description,
-    this.delete = false,
+    this.remove = false,
     this.status = false,
     required this.createdAt,
     required this.updatedAt,
   });
 
-  void toogleStatus() => delete = !delete;
-  void toogleCompleted() => status = !status;
-  set setUpdatedAt(DateTime newUpdatedAt) => updatedAt = newUpdatedAt;
-
   Task copyWith({
     String? id,
     String? title,
     String? description,
-    bool? delete,
+    bool? remove,
     bool? status,
     DateTime? createdAt,
     DateTime? updatedAt,
@@ -36,11 +48,10 @@ class Task {
       id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
-      delete: delete ?? this.delete,
+      remove: remove ?? this.remove,
       status: status ?? this.status,
       createdAt: createdAt ?? this.createdAt,
       updatedAt: updatedAt ?? this.updatedAt,
     );
   }
-  
 }
